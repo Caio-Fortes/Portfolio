1f16f899db44500959d2a49c8f44e470aefc747d
Caio <caiofortesprofissional@gmal.com>
2024-07-26 18:55:01 -0300

    sobre mim

--- project/src/components/Profile.vue
+++ project/src/components/Profile.vue
@@ -2,8 +2,15 @@
     import '../css/components/Profile.scss';
 
     export default{
-        data(){
-            return{
+        methods:{
+            downloadCv() {
+                const pdfPath = '/CV.pdf';
+                const link = document.createElement('a');
+                link.href = pdfPath;
+                link.download = 'CaioFortesCv.pdf';
+                document.body.appendChild(link);
+                link.click();
+                document.body.removeChild(link);
             }
         }
     }
@@ -17,13 +24,15 @@
         <div id="profileNameDescription">
             <h3>Sobre mim</h3>
             <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer tellus est,
-                finibus ut congue sed, faucibus ut dui. Sed congue nisl dolor, id dapibus
-                leo elementum posuere. Ut aliquam metus quis laoreet elementum. 
+                Meu nome é Caio Fortes dos Santos e possuo 2 anos de experiência no mercado de 
+                trabalho como desenvolvedor web, além de 8 anos de experiência acadêmica adquirida 
+                em cursos técnicos e profissionalizantes. Sou graduado em Análise e Desenvolvimento de 
+                Sistemas e venho aprimorando minhas habilidades em tecnologia desde 2016, 
+                quando iniciei minha formação em cursos técnicos na área.
             </p>
             <p> <strong>Email:</strong> caiofortesprofissional@gmail.com</p>
             <p> <strong>Contato:</strong> (35) 99849-8003</p>
-            <button id="btnDownloadCv">
+            <button id="btnDownloadCv" @click="downloadCv">
                 Download Cv
             </button>
         </div>
